310cb8db40ddbe21f48842ebad3376ee991dc249
Vanesa Acevedo Grajales <nesa6423@gmail.com>
2024-11-24 21:29:46 -0500

    Finalización código

--- Arrays_Methodos/Arrays_Methodos/Program.cs
+++ Arrays_Methodos/Arrays_Methodos/Program.cs
@@ -4,13 +4,14 @@
     {
         //45.Realizar un programa en C# que declare un array con N enteros, a continuación, lo inicialice con valores aleatorios (del 1 al 10) y posteriormente en otros 2 arrays guardar el cuadrado de cada uno de los números número y cubo de ese número. Muestre en pantalla cada elemento del primer vector de enteros junto con su cuadrado y su cubo.
 
-        Console.WriteLine("Ingrese el número de elementos: *");
+        Console.WriteLine("Ingrese el número de elementos: ");
         int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("\n");
 
         //Declaramos los 3 Arrays
         int[] numeros = new int[n];
-        int[] cuadrados = new int[n];
-        int[] cubos = new int[n];
+        double[] cuadrados = new double[n];
+        double[] cubos = new double[n];
 
         FillArray(n, numeros);
         CalculateOperations(n, numeros, cuadrados, cubos);
@@ -18,22 +19,31 @@
 
     }
 
-    private static void ShowResults(int n, int[] numeros, int[] cuadrados, int[] cubos)
+    private static void ShowResults(int n, int[] numeros, double[] cuadrados, double[] cubos)
     {
-        throw new NotImplementedException();
+        Console.WriteLine("Numero\tCuadrado\tCubo");
+
+        for (int i = 0; i < n; i++)
+            Console.WriteLine($"{numeros[i]}\t{cuadrados[i]}\t\t{cubos[i]}");
+      
     }
 
-    private static void CalculateOperations(int n, int[] numeros, int[] cuadrados, int[] cubos)
+    private static void CalculateOperations(int n, int[] numeros, double[] cuadrados, double[] cubos)
     {
-        
+        for (int i = 0; i < n; i++)
+        {
+            cuadrados[i] = Math.Pow(numeros[i], 2);
+            cubos[i] = Math.Pow(numeros[i], 3);
+        }
     }
 
     private static void FillArray(int n, int[] numeros)
     {
         Random random = new Random();
+
         for (int i = 0; i < n; i++)
         {
-            int number = random.Next(1, 11);
+            numeros[i]= random.Next(1, 11);
             //numeros[i] = number;
         }
 
